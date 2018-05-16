5ef8df515d8a415075555b5cf1bbe4bb9ba6eba5
uniquezl <zhulin_8256@sina.cn>
2018-05-16 11:43:47 +0800

    modification

--- test/ViewController.m
+++ test/ViewController.m
@@ -18,22 +18,6 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    
-    
-    
-
-    //dispatch_release(zlQueue);
-//    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
-//    dispatch_group_t group = dispatch_group_create();
-//    dispatch_group_async(group, queue, ^{NSLog(@"blk0");});
-//    dispatch_group_async(group, queue, ^{NSLog(@"blk1");});
-//    dispatch_group_async(group, queue, ^{NSLog(@"blk2");});
-//    dispatch_group_notify(group, dispatch_get_main_queue(), ^{NSLog(@"done");});
-//
-//    dispatch_queue_t zlQueue = dispatch_queue_create("com.zl.tantan", DISPATCH_QUEUE_CONCURRENT);
-//
-//    dispatch_async(zlQueue, ^{NSLog(@"zlzlzzl");});
-    
     dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
     dispatch_apply(10, queue, ^(size_t index) { NSLog(@"%zu", index);
     }); NSLog(@"done");
